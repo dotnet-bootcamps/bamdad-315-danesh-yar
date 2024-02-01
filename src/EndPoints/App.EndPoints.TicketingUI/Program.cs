a04cbccdc421e0474ae263e16e1a0f8f237a2b12
msavarian <sav68.net@gmail.com>
2024-02-01 13:09:11 +0330

    add ui

--- src/EndPoints/App.EndPoints.TicketingUI/Program.cs
+++ src/EndPoints/App.EndPoints.TicketingUI/Program.cs
@@ -3,6 +3,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.MapControllerRoute(name: "areas", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
