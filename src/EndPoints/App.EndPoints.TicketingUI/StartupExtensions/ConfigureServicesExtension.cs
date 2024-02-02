8b903366e08b20603a29ba7728fc1d4a1b1deff3
Ozymandias95 <-help>
2024-02-02 12:09:22 +0330

    manage Connctionstring and add ConfigureServicesExtension class

---
+++ src/EndPoints/App.EndPoints.TicketingUI/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,12 @@
+﻿using App.Infrastructures.Db.SqlServer.Ef.DbCtxs;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.EndPoints.TicketingUI.StartupExtensions;
+
+public static class ConfigureServicesExtension
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(option => option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
