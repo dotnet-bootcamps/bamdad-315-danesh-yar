8d834bd70618d2194bc012cdd6413747b605b847
Ozymandias95 <-help>
2024-02-02 12:41:25 +0330

    Fix show property display in create ticket form

--- src/Core/App.Core.Domain/Entities/Ticket.cs
+++ src/Core/App.Core.Domain/Entities/Ticket.cs
@@ -8,46 +8,46 @@ public partial class Ticket
     public int Id { get; set; }
 
 
-    [Display(Name = "")]
+    [Display(Name = "موضوع")]
     public string Subject { get; set; } = null!;
 
 
-    [Display(Name = "")]
+    [Display(Name = "شناسه دسته بندی")]
     public byte CategoryId { get; set; }
 
 
-    [Display(Name = "")]
+    [Display(Name = "شناسه اولویت")]
     public byte PriorityId { get; set; }
 
 
-    [Display(Name = "")]
+    [Display(Name = "شناسه وضعیت")]
     public byte CurrentStatusId { get; set; }
 
 
-    [Display(Name = "")]
+    [Display(Name = "توضیحات")]
     public string Description { get; set; } = null!;
 
 
-    [Display(Name = "")]
+    [Display(Name = "نام درخواست کننده")]
     public int SubmitedBy { get; set; }
 
 
-    [Display(Name = "")]
+    [Display(Name = "زمان ثبت")]
     public DateTime SubmittedAt { get; set; }
 
 
-    [Display(Name = "")]
+    [Display(Name = "دسته بندی")]
     public virtual TicketCategory Category { get; set; } = null!;
 
 
-    [Display(Name = "")]
+    [Display(Name = "وضعیت")]
     public virtual TicketStatus CurrentStatus { get; set; } = null!;
 
 
-    [Display(Name = "")]
+    [Display(Name = "اولویت")]
     public virtual TicketPriority Priority { get; set; } = null!;
 
 
-    [Display(Name = "")]
+    [Display(Name = "تاریخچه")]
     public virtual ICollection<TicketHistory> TicketHistories { get; set; } = new List<TicketHistory>();
 }
